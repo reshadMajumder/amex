ecb0987c257532240ad418d67876174b1dfcf006
reshadMajumder <reshad22205341197@diu.edu.bd>
2026-01-29 13:36:51 +0600

    company account created and can be added with any users

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,11 +4,15 @@ from django.contrib import admin
 from django.urls import include, path   
 from django.urls import path
 
-from .views import UserAccountsView
+from .views import UserAccountsView, AccountView
 
 urlpatterns = [
     path('admin/user-get-or-create/', UserAccountsView.as_view(), name='accounts-home'),
     path('admin/user-get-or-create/<int:id>/', UserAccountsView.as_view(), name='accounts-detail'),
     path('admin/user-update-or-delete/<int:id>/', UserAccountsView.as_view(), name='accounts-delete'),
+
+    path('admin/account-get-or-create/', AccountView.as_view(), name='accounts-home'),
+    path('admin/account-get-or-create/<int:id>/', AccountView.as_view(), name='accounts-detail'),
+    path('admin/account-update-or-delete/<int:id>/', AccountView.as_view(), name='accounts-delete'),
     
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -9,8 +9,8 @@ from rest_framework.decorators import APIView
 
 from rest_framework.permissions import IsAuthenticated  
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .models import User
-from .serializers import UserSerializer
+from .models import Account, User
+from .serializers import AccountSerializer, UserSerializer
 from .permissions import IsAdminUser, IsSuperAdminUser
 from rest_framework.response import Response
 
@@ -73,3 +73,59 @@ class UserAccountsView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=500)
         
+
+
+class AccountView(APIView):
+    '''
+    create company accounts,
+    get company accounts list
+    get the details of company
+
+    '''
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser, IsSuperAdminUser]
+
+    def get(self, request,id=None):
+        try:
+            if id:
+                account = Account.objects.get(id=id)
+                serializer = AccountSerializer(account)
+                return Response({'Account': serializer.data}, status=200)
+            else:
+                accounts = Account.objects.all()
+                serializer = AccountSerializer(accounts, many=True)
+                return Response({'Accounts': serializer.data}, status=200)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
+        
+
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = AccountSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': 'Company Account created successfully', 'Account': serializer.data}, status=201)
+            return Response({'errors': serializer.errors}, status=400)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
+        
+    def put(self, request, id):
+        try:
+            account = Account.objects.get(id=id)
+            data = request.data
+            serializer = AccountSerializer(account, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': 'Company Account updated successfully', 'Account': serializer.data}, status=200)
+            return Response({'errors': serializer.errors}, status=400)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
+        
+    def delete(self, request, id):
+        try:
+            account = Account.objects.get(id=id)
+            account.delete()
+            return Response({'message': 'Company Account deleted successfully'}, status=200)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
